b6d21d1139124f3aa6180e23e2d3d9791d3eec81
DhirajLucidux <88828651+DhirajLucidux@users.noreply.github.com>
2022-11-04 19:48:11 +0530

    updated code on stories module

--- DemoFacebook/src/app/modules/stories/create-stories/create-stories.component.ts
+++ DemoFacebook/src/app/modules/stories/create-stories/create-stories.component.ts
@@ -2,66 +2,68 @@ import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Subject, takeUntil } from 'rxjs';
 import { StorieService } from 'src/app/common/services/storie.service';
 import { UserService } from 'src/app/common/services/user.service';
 
 @Component({
   selector: 'app-create-stories',
   templateUrl: './create-stories.component.html',
-  styleUrls: ['./create-stories.component.scss']
+  styleUrls: ['./create-stories.component.scss'],
 })
 export class CreateStoriesComponent implements OnInit {
-
   constructor(
-    private userservice:UserService,
-    private storymanage:StorieService,
-    private router:Router,
-    private toastr:ToastrService,
+    private userservice: UserService,
+    private storymanage: StorieService,
+    private router: Router,
+    private toastr: ToastrService,
     private location: Location
-  ) { }
- data:any;
- storysrc:any;
- file:any;
+  ) {}
+  loginUserDetails: any;
+  storysrc: any;
+  file: any;
+  destoy$: Subject<void> = new Subject<void>();
+
   ngOnInit(): void {
-    this.userservice.currentLoginUser.subscribe( (res: any) =>{
-      console.log(res);
-      this.data=res;
-    });
+    this.userservice.currentLoginUser
+      .pipe(takeUntil(this.destoy$))
+      .subscribe((res: any) => {
+        if (res) {
+          console.log(res);
+          this.loginUserDetails = res;
+        }
+      });
   }
 
-  onFilechangestory(e:any) {
-    // console.log('hello');
-    // console.log(e);
-    if(e.target.files){
+  onFilechangestory(e: any) {
+    if (e.target.files) {
       const reader = new FileReader();
       reader.readAsDataURL(e.target.files[0]);
-      reader.onload = (event:any)=>{  
-        this.file=e.target.files[0];
-        // console.log(this.file)
-        // console.log(this.imageSrc)
+      reader.onload = (event: any) => {
+        this.file = e.target.files[0];
         this.storysrc = event.target.result;
-        // console.log(this.storysrc);
-
-      }
-
+      };
     }
   }
-  oncreatestory(){
+  oncreatestory() {
+    let formdata = new FormData();
+    formdata.append('user_id', this.loginUserDetails._id);
+    formdata.append('storyUrl', this.file);
 
-    let formdata = new FormData;
-    // formdata.append('storyText',this.createPost.get('storyText')?.value);
-    formdata.append('user_id',this.data._id);
-    formdata.append('storyUrl',this.file);
-
-      this.storymanage.createstory(formdata).subscribe((res)=>{
+    this.storymanage.createstory(formdata).subscribe(
+      (res) => {
         this.storysrc = '';
-        this.file='';
-        this.toastr.success('Story Created Successfully ','Success!');
+        this.file = '';
+        this.toastr.success('Story Created Successfully ', 'Success!');
         this.location.back();
-            },(err)=>{
-
+      },
+      (err) => {
         console.log(err);
-      })
-}
+      }
+    );
+  }
 
+  ngOnDestroy() {
+    this.destoy$.next();
+  }
 }
--- DemoFacebook/src/app/modules/stories/stories.component.ts
+++ DemoFacebook/src/app/modules/stories/stories.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { AuthService } from 'src/app/common/services/auth.service';
 import { SharedDataService } from 'src/app/common/services/shared-data.service';
 import { UserService } from 'src/app/common/services/user.service';
@@ -7,32 +6,30 @@ import { UserService } from 'src/app/common/services/user.service';
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
-  styleUrls: ['./stories.component.scss']
+  styleUrls: ['./stories.component.scss'],
 })
 export class StoriesComponent implements OnInit {
-
-  
-
   constructor(
-    private route:Router,
-    private userservice:UserService,
-    private sharedService:SharedDataService,
-    private authservice:AuthService
-    ) { }
- data:any;
- ngOnInit(): void {
-  this.sharedService.changeTitle('Deskbook | Stories');
-   this.getcurrentuser();
- }
+    private userservice: UserService,
+    private sharedService: SharedDataService,
+    private authservice: AuthService
+  ) {}
+  data: any;
+  ngOnInit(): void {
+    this.sharedService.changeTitle('Deskbook | Stories');
+    this.getcurrentuser();
+  }
 
- token:any;
- getcurrentuser(){
-   this.authservice.getUserProfile().subscribe(res=>{
-     console.log(res);
-     localStorage.setItem('accountHolder',JSON.stringify(res));
-     this.userservice.currentLoginUser.next(res);
- });
+  token: any;
+  getcurrentuser() {
+    this.authservice.getUserProfile().subscribe((res) => {
+      console.log(res);
+      localStorage.setItem('accountHolder', JSON.stringify(res));
+      this.userservice.currentLoginUser.next(res);
+    });
+  }
 
-}
-
+  ngOnDestroy() {
+    this.userservice.currentLoginUser.next('');
+  }
 }
