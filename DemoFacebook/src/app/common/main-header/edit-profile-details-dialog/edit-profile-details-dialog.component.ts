514446506e1a4babab380765832de7f5a0264d96
DhirajLucidux <88828651+DhirajLucidux@users.noreply.github.com>
2022-11-13 15:15:39 +0530

    edit profile componant edit

--- DemoFacebook/src/app/common/main-header/edit-profile-details-dialog/edit-profile-details-dialog.component.ts
+++ DemoFacebook/src/app/common/main-header/edit-profile-details-dialog/edit-profile-details-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
+import { Subject, takeUntil } from 'rxjs';
 import { SharedDataService } from '../../services/shared-data.service';
 import { UserService } from '../../services/user.service';
 
@@ -21,33 +22,33 @@ export class EditProfileDetailsDialogComponent implements OnInit {
     private sharedservice:SharedDataService
   ) { }
 
-   ngAfterViewInit(){
-    // this.sendMessageInput.nativeElement.focus();
-  }
   
   editProfile:any;
-    data:any;
+  currentLoginUserDetails:any;
+  CoverPhoto:any;
+  Destroy$ : Subject<void> = new Subject<void>();
+
 
   ngOnInit(): void {
 
-    this.userservice.currentLoginUser.subscribe( (res: any) =>{
-      console.log(res);
-      this.data=res;
-      if(this.data){
-        this.oninitgetdata();
-      }
-      if(this.data.user_info){
+    this.userservice.currentLoginUser.pipe(takeUntil(this.Destroy$)).subscribe( (res: any) =>{
+      if(res){
+      this.currentLoginUserDetails=res;
+      if(this.currentLoginUserDetails.user_info){
         this.editProfile = this.fb.group({
-          'workplace':this.data.user_info.workplace ? this.data.user_info.workplace : '',
-          'highSchool':this.data.user_info.highSchool ? this.data.user_info.highSchool:'',
-          'university':this.data.user_info.university ?this.data.user_info.university:'' ,
+          'workplace':this.currentLoginUserDetails.user_info.workplace ? this.currentLoginUserDetails.user_info.workplace : '',
+          'highSchool':this.currentLoginUserDetails.user_info.highSchool ? this.currentLoginUserDetails.user_info.highSchool:'',
+          'university':this.currentLoginUserDetails.user_info.university ?this.currentLoginUserDetails.user_info.university:'' ,
           'CoverPhoto':'',
-          'currentCity':this.data.user_info.currentCity ? this.data.user_info.currentCity:'',
-          'homeTown':this.data.user_info.homeTown ?this.data.user_info.homeTown:'',
-          'relation':this.data.user_info.relation?this.data.user_info.relation:'',
-          'user_id':this.data._id
+          'currentCity':this.currentLoginUserDetails.user_info.currentCity ? this.currentLoginUserDetails.user_info.currentCity:'',
+          'homeTown':this.currentLoginUserDetails.user_info.homeTown ?this.currentLoginUserDetails.user_info.homeTown:'',
+          'relation':this.currentLoginUserDetails.user_info.relation?this.currentLoginUserDetails.user_info.relation:'',
+          'user_id':this.currentLoginUserDetails._id
       
       });
+      if(this.currentLoginUserDetails.user_info.CoverPhoto){
+        this.CoverPhoto = 'http://localhost:2000'+this.currentLoginUserDetails.user_info.CoverPhoto;
+      }
       }else{
         this.editProfile = this.fb.group({
           'workplace':'',
@@ -57,35 +58,12 @@ export class EditProfileDetailsDialogComponent implements OnInit {
           'currentCity':'',
           'homeTown':'',
           'relation':'',
-          'user_id':this.data._id
-      
+          'user_id':this.currentLoginUserDetails._id
       });
       }
-      
+    }
     });
   }
-  
-  CoverPhoto:any;
-  currentUser:any;
-  oninitgetdata(){
-
-    this.userservice.currentVisitedUser.subscribe((res: any)=>{
-      this.currentUser = res;
-      console.log(this.currentUser)
-      if(this.currentUser.user_info){
-        if(this.currentUser.user_info.CoverPhoto){
-          this.CoverPhoto = 'http://localhost:2000'+this.currentUser.user_info.CoverPhoto;
-        }
-  
-      }
-      if(this.currentUser){
-      }
-    })
-   
-  }
-  friendsId:any = [];
-  m=0;
-
 
   file:any;
   onChangeCoverPhoto(e:any) {
@@ -94,44 +72,37 @@ export class EditProfileDetailsDialogComponent implements OnInit {
       reader.readAsDataURL(e.target.files[0]);
       reader.onload = (event:any)=>{  
         this.file=e.target.files[0];
-        console.log(this.file)
-        // console.log(this.imageSrc)
         this.CoverPhoto = event.target.result;
-
       }
-
     }
   }
+
   oneditprofile(){
-
-
       let formdata = new FormData();
       formdata.append('workplace',this.editProfile.get('workplace').value);
       formdata.append('highSchool',this.editProfile.get('highSchool').value);
       formdata.append('university',this.editProfile.get('university').value);
-if(this.file){
-  formdata.append('CoverPhoto',this.file);
 
-}
+      if(this.file){
+        formdata.append('CoverPhoto',this.file);
+      }
+
       formdata.append('currentCity',this.editProfile.get('currentCity').value);
       formdata.append('homeTown',this.editProfile.get('homeTown').value);
       formdata.append('relation',this.editProfile.get('relation').value);
       formdata.append('user_id',this.editProfile.get('user_id').value);
 
-
-
-    this.userservice.createUserInfo(formdata).subscribe(res=>{
-      console.log(res);
-      // this.editProfile.reset();
-      this.CoverPhoto='';
-      this.file=null;
-      this.toastr.success('Profile Updated Successfully', 'Success!');
+    this.userservice.createUserInfo(formdata).subscribe((res:any)=>{
+      this.toastr.success(res, 'Success!');
       this.sharedservice.editProfileSave.next(true);
+      this.sharedservice.updatedUserDetails.next(true);
       this.dialogref.close();
     },err=>{
       console.log(err);
     })
-  } 
-
-
+  }
+  
+  ngOnDestroy(){
+    this.Destroy$.next()
+  }
 }
