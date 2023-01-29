0646ef6b5810002587fefd90f12a69ef9a101f21
dhiraj_thakare_office <88828651+DhirajLucidux@users.noreply.github.com>
2023-01-29 14:18:52 +0530

    change project name wording

--- DemoFacebook/README.md
+++ DemoFacebook/README.md
@@ -1,4 +1,4 @@
-# DemoFacebook
+# deskbook
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.6.
 
--- DemoFacebook/src/app/app.component.spec.ts
+++ DemoFacebook/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DemoFacebook'`, () => {
+  it(`should have as title 'deskbook'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DemoFacebook');
+    expect(app.title).toEqual('deskbook');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('DemoFacebook app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('deskbook app is running!');
   });
 });
