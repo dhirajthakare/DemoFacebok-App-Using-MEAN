d83ee2c44ca3ee1ef3ffed35bdabf4bb314cad89
dhiraj thakare <dhiraj.t@kelpglobal.com>
2024-03-24 16:34:33 +0530

    drag and drop added in friends nav

--- DemoFacebook/src/app/modules/friends/friends-sidebar/friends-sidebar.component.ts
+++ DemoFacebook/src/app/modules/friends/friends-sidebar/friends-sidebar.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { friendNav } from '../interface/friend.interface';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-friends-sidebar',
   templateUrl: './friends-sidebar.component.html',
-  styleUrls: ['./friends-sidebar.component.scss']
+  styleUrls: ['./friends-sidebar.component.scss'],
 })
 export class FriendsSidebarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  navNode:friendNav[]= [
+    {
+      route: '/friends',
+      icon: 'user-group-man-man.png',
+      value: 'Home',
+    },
+    {
+      route: 'friend-request',
+      icon: 'user-clock.png',
+      value: 'Friend Requests',
+    },
+    {
+      route: '/friends',
+      icon: 'add-user-male.png',
+      value: 'Suggestions',
+    },
+    {
+      route: 'all-friends',
+      icon: 'checked-user-male.png',
+      value: 'All Friends',
+    },
+    {
+      route: '/friends',
+      icon: 'gift--v1.png',
+      value: 'Birthday',
+    },
+    {
+      route: '/friends',
+      icon: 'change-user-male.png',
+      value: 'Custom List',
+    },
+  ];
 
-  ngOnInit(): void {
+  drop(event: CdkDragDrop<friendNav[]>) {
+    moveItemInArray(this.navNode, event.previousIndex, event.currentIndex);
   }
 
+  ngOnInit(): void {}
 }
