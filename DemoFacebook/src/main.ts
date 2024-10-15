f6763a846759354197f6b307c3b29cc729e2aa90
dhiraj thkare <dhiraj9900@gmail.com>
2024-10-15 15:39:57 +0530

    added some code to applay -hmr statergy

--- DemoFacebook/src/main.ts
+++ DemoFacebook/src/main.ts
@@ -3,9 +3,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+declare const module: NodeModule & { hot?: { accept: () => void; dispose: (callback: () => void) => void } };
 if (environment.production) {
   enableProdMode();
+} else if (module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => {
+    const appRoot = document.querySelector('app-root');
+    if (appRoot) {
+      appRoot.innerHTML = '';
+    }
+  });
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
